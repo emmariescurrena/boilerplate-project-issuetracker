993a34da62a2265ee9d708a145f9dea0774e5fcd
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-16 09:02:17 -0300

    First commit

--- routes/api.js
+++ routes/api.js
@@ -2,26 +2,25 @@
 
 module.exports = function (app) {
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+    app.route('/api/issues/:project')
+
+        .get(function (req, res) {
+            let project = req.params.project;
+        })
+
+        .post(function (req, res) {
+            let project = req.params.project;
+
+        })
+
+        .put(function (req, res) {
+            let project = req.params.project;
+
+        })
+
+        .delete(function (req, res) {
+            let project = req.params.project;
+
+        });
+
 };
